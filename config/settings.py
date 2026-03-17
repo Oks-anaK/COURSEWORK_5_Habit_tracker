76acb0b6121f6cd54bb3611a902f66962160419f
Oksana Konkina <oksanka.808@mail.ru>
2026-03-17 03:06:12 +0300

    Edit settings, .env (allowed_hosts)

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', 'localhost').split(',') if host.strip()]
 
 
 # Application definition
